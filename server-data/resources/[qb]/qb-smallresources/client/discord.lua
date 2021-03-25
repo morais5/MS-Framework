f081adeb27288c5105a74a7ffdb12ba84b64e022
Miguel Morais <miguelafilipe7@gmail.com>
2021-03-25 18:52:23 +0000

    Alteração de discord e id

--- server-data/resources/[qb]/qb-smallresources/client/discord.lua
+++ server-data/resources/[qb]/qb-smallresources/client/discord.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function()
 	while true do
         --This is the Application ID (Replace this with you own)
-		SetDiscordAppId(779511700927807519)
+		SetDiscordAppId(779511412427807519)
 
         --Here you will have to put the image name for the "large" icon.
 		SetDiscordRichPresenceAsset('logo')
@@ -9,13 +9,13 @@ Citizen.CreateThread(function()
         --(11-11-2018) New Natives:
 
         --Here you can add hover text for the "large" icon.
-        SetDiscordRichPresenceAssetText('https://discord.io/criminalrp')
+        SetDiscordRichPresenceAssetText('https://discord.gg/yMShaH5Kae')
         
         --Here you will have to put the image name for the "small" icon.
         --SetDiscordRichPresenceAssetSmall('logo-mk3')
 
         --Here you can add hover text for the "small" icon.
-        SetDiscordRichPresenceAssetSmallText('https://discord.io/criminalrp')
+        SetDiscordRichPresenceAssetSmallText('https://discord.gg/yMShaH5Kae')
 
         --It updates every one minute just in case.
 		Citizen.Wait(60000)
