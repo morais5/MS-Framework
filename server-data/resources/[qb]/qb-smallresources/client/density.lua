26d1b2c6f08ec124f35f476e65a6de61d8f30c94
Miguel Morais <miguelafilipe7@gmail.com>
2021-03-25 18:46:40 +0000

    Alteração do script density.lua

--- server-data/resources/[qb]/qb-smallresources/client/density.lua
+++ server-data/resources/[qb]/qb-smallresources/client/density.lua
@@ -1,38 +1,28 @@
 Citizen.CreateThread(function()
-	while true do
-		SetVehicleDensityMultiplierThisFrame(0.0) -- tava a 0.1
-		SetPedDensityMultiplierThisFrame(0.0) -- set npc/ai peds density to 0
-		SetRandomVehicleDensityMultiplierThisFrame(0.0) -- set random vehicles (car scenarios / cars driving off from a parking spot etc.) to 0
-		SetParkedVehicleDensityMultiplierThisFrame(0.0) -- set random parked vehicles (parked car scenarios) to 0
-		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0) -- set random npc/ai peds or scenario peds to 0
-		--SetGarbageTrucks(false) -- Stop garbage trucks from randomly spawning
-		--SetRandomBoats(false) -- Stop random boats from spawning in the water.
-		--SetCreateRandomCops(false) -- disable random cops walking/driving around.
-		--SetCreateRandomCopsNotOnScenarios(false) -- stop random cops (not in a scenario) from spawning.
-		--SetCreateRandomCopsOnScenarios(false) -- stop random cops (in a scenario) from spawning.
-
---abaixo é da cena do minimapa codigo postal 
-		if IsPedOnFoot(GetPlayerPed(-1)) then 
-			SetRadarZoom(1100)
-		elseif IsPedInAnyVehicle(GetPlayerPed(-1), true) then
-			SetRadarZoom(1100)
-		end
---acima é da cena do minimapa codigo postal 
-
---abaixo é pra desativar os police car rewards
-	DisablePlayerVehicleRewards(PlayerId())	
---acima é pra desativar os police car rewards
-
-		Citizen.Wait(3)
-	end
+    while true do
+	Citizen.Wait(0)
+        SetVehicleDensityMultiplierThisFrame(0.2) --Seleciona densidade do trafico
+	SetPedDensityMultiplierThisFrame(0.1) --seleciona a densidade de Npc
+	SetRandomVehicleDensityMultiplierThisFrame(0.3) --seleciona a densidade de viaturas estacionadas a andar etc
+	SetParkedVehicleDensityMultiplierThisFrame(0.3) --seleciona a densidade de viaturas estacionadas
+	SetScenarioPedDensityMultiplierThisFrame(1.0, 1.0) --seleciona a densidade de Npc a andar pela cidade
+	SetGarbageTrucks(false) --Desactiva os Camioes do Lixo de dar Spawn Aleatoriamente
+	SetRandomBoats(false) --Desactiva os Barcos de dar Spawn na agua
+        SetCreateRandomCops(false) --Desactiva a Policia a andar pela cidade
+	SetCreateRandomCopsNotOnScenarios(false) --Para o Spanw Aleatorio de Policias Fora do Cenario
+	SetCreateRandomCopsOnScenarios(false) --Para o Spanw Aleatorio de Policias no Cenario
+        DisablePlayerVehicleRewards(PlayerId()) --Nao mexer --> Impossibilita que os players possam ganhar armas nas viaturas da policia e ems
+        RemoveAllPickupsOfType(0xDF711959) --Carbine rifle
+        RemoveAllPickupsOfType(0xF9AFB48F) --Pistol
+        RemoveAllPickupsOfType(0xA9355DCD) --Pumpshotgun
+        local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
+	ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
+	RemoveVehiclesFromGeneratorsInArea(x - 500.0, y - 500.0, z - 500.0, x + 500.0, y + 500.0, z + 500.0);
+        HideHudComponentThisFrame(14)-- Remover Mira
+        RemoveMultiplayerHudCash(0x968F270E39141ECA) -- Remove o Dinheiro Original do Gta
+        RemoveMultiplayerBankCash(0xC7C6789AA1CFEDD0) --Remove o Dinheiro Original do Gta Que esta no Banco
+	for i = 1, 15 do
+        EnableDispatchService(i, false)-- Disabel Dispatch
+      end
+   end
 end)
-
---abaixo é da cena do minimapa codigo postal 
-Citizen.CreateThread(function()
-    SetMapZoomDataLevel(0, 0.96, 0.9, 0.08, 0.0, 0.0) -- Level 0
-    SetMapZoomDataLevel(1, 1.6, 0.9, 0.08, 0.0, 0.0) -- Level 1
-    SetMapZoomDataLevel(2, 8.6, 0.9, 0.08, 0.0, 0.0) -- Level 2
-    SetMapZoomDataLevel(3, 12.3, 0.9, 0.08, 0.0, 0.0) -- Level 3
-    SetMapZoomDataLevel(4, 22.3, 0.9, 0.08, 0.0, 0.0) -- Level 4
-end)
-
