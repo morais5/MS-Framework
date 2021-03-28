969ec6f0fe7da7167d01c328949fb47b9a0ac2e7
Ádám Simon <adamsimondev@gmail.com>
2021-03-28 15:30:09 +0200

    Revert "Sniper crosshair fix"
    This reverts commit 99b56decdac0db54e1e28fc5b8e21a18b17374ae.

--- server-data/resources/[qb]/qb-smallresources/client/hudcomponents.lua
+++ server-data/resources/[qb]/qb-smallresources/client/hudcomponents.lua
@@ -26,15 +26,6 @@
 ]]--
 
 Citizen.CreateThread(function()
-    while true do
-        if not IsAimCamActive() or not IsFirstPersonAimCamActive() then
-            HideHudComponentThisFrame(14)
-        end
-        Citizen.Wait(0)
-    end
-end)
-
-Citizen.CreateThread(function()
 	while true do
 		HideHudComponentThisFrame(1)
 		HideHudComponentThisFrame(2)
